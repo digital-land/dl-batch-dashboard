a5e4062e534eac53aac5119e79a39a8550bd4242
Kishan Patel <16578929+ksp37@users.noreply.github.com>
2021-11-03 16:33:15 +0000

    Implemented background fetch and loading page.

--- config/config.py
+++ config/config.py
@@ -2,10 +2,14 @@
 
 import pathlib
 import os
+from datetime import timedelta
 
 
 class Config(object):
     PROJECT_ROOT = pathlib.Path(__file__).parent.parent.absolute()
+    SESSION_TYPE = 'filesystem'
+    SESSION_PERMANENT = True
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
     SECRET_KEY = os.getenv("SECRET_KEY")
     if "AWS_ACCESS_KEY_ID" not in os.environ or "AWS_SECRET_ACCESS_KEY" not in os.environ:
         raise KeyError("Missing AWS credential environment variables")
